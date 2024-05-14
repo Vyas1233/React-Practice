9a3a1c6cebb6174597cd9e4eb54052b886bee4ba
Vyas1233 <vyasnarasimha@gmail.com>
2024-05-14 16:51:20 +0530

    lazy loading added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from './components/Header';
 import Body from './components/Body';
@@ -7,6 +7,9 @@ import About from './components/About';
 import Contact from './components/Contact';
 import Error from './components/Error';
 import RestaurantMenu from './components/RestaurantMenu';
+import Shimmer from './components/Shimmer';
+
+const Grocery = lazy(() => import('./components/Grocery'));
 
 
 const AppLayout = () => {
@@ -18,6 +21,7 @@ const AppLayout = () => {
     )
 
 }
+
 
 const appRouter = createBrowserRouter([
     {
@@ -34,6 +38,10 @@ const appRouter = createBrowserRouter([
             },
             { path:"/contact",
               element: <Contact />
+            
+            },
+            { path:"/grocery",
+              element: <Suspense fallback={<h1>Loading..</h1>}><Grocery /></Suspense>
             
             },
             { path:"/restaurants/:resId",
---
+++ src/components/Grocery.js
@@ -0,0 +1,10 @@
+const Grocery = () => {
+    return(
+        <div>
+            <h1>Welcome to Grocery Store.</h1>
+            <p>Sorry for the inconvinience, this page will be available soon.</p>
+        </div>
+    )
+}
+
+export default Grocery;
